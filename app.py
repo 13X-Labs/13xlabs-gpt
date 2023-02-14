2fb2bf78685c3e7fdd40f34e9a94a4e389d8c7b0
Hồ Ngọc Hải <hongochai10@icloud.com>
2023-02-14 19:07:24 +0700

    update data

--- app.py
+++ app.py
@@ -3,17 +3,23 @@ from decouple import config
 import openai
 import tensorflow as tf
 
+# Declare Flask application
 app = Flask(__name__)
 
+# Set OpenAI API key
 openai.api_key = config('OPENAI_API_KEY')
 
-model_path = "models/355M"
+# Path to GPT-2 model
+model_path = "data/small-117M"
 
+# Load GPT-2 model
 model = tf.saved_model.load(model_path)
 
+# Define input and output tensors
 input_text = model.signatures["serving_default"].inputs[0]
 output = model.signatures["serving_default"].outputs["output_0"]
 
+# Define API endpoint
 @app.route('/api/complete', methods=['POST'])
 def complete():
     data = request.get_json()
@@ -21,11 +27,14 @@ def complete():
     length = data.get('length', 100)
     temperature = data.get('temperature', 0.5)
 
+	# Use GPT-2 model to generate text
     output_text = model(input_text=tf.constant([prompt]), length=tf.constant(length), temperature=tf.constant(temperature))["output_0"].numpy()[0].decode('utf-8')
 
+	# Return generated text as JSON
     return jsonify({
         "output": output_text
     })
 
+# Start the application
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
